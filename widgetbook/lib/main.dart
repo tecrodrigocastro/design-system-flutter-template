b1a4c572f326981c645d088f09d1f0d6e8787805
Rodrigo Castro <tec.rodrigocastro@gmail.com>
2026-08-29 20:54:55 -0300

    feat(widgetbook): use DsTheme for the theme addon
    Replaces vanilla ThemeData.light/dark with the package's own theme.

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
@@ -25,8 +26,8 @@ class WidgetbookGalleryApp extends StatelessWidget {
       addons: [
         MaterialThemeAddon(
           themes: [
-            WidgetbookTheme(name: 'Light', data: ThemeData.light(useMaterial3: true)),
-            WidgetbookTheme(name: 'Dark', data: ThemeData.dark(useMaterial3: true)),
+            WidgetbookTheme(name: 'Light', data: DsTheme.light),
+            WidgetbookTheme(name: 'Dark', data: DsTheme.dark),
           ],
         ),
         ViewportAddon(Viewports.all),
